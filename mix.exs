3f9bad03c9866acac6f6d874334eab66018712f1
Joel C <joel@vorce.se>
2017-10-05 22:42:21 +0200

    Bump version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rushie.Mixfile do
   def project do
     [
       app: :rushie,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
